5b83d4b089d25e16e5b12082a2e17df5120da5b3
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-27 21:27:05 +0530

    Remove unnecessary casts
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,47 +1,36 @@
 package com.example.musicplayer;
 
 import android.media.MediaPlayer;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-
     private MediaPlayer mediaPlayer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mediaPlayer = MediaPlayer.create(this, R.raw.nitkslamdunk);
-        Button playButton = (Button)findViewById(R.id.play_button);
+        Button playButton = findViewById(R.id.play_button);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mediaPlayer!=null) {
+                if (mediaPlayer != null) {
                     mediaPlayer.start();
-
                 }
             }
         });
-
-        Button pauseButton = (Button)findViewById(R.id.pause_button);
+        Button pauseButton = findViewById(R.id.pause_button);
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mediaPlayer!=null) {
+                if (mediaPlayer != null) {
                     mediaPlayer.pause();
                 }
             }
         });
-
-
     }
-
-
-
-
 }
